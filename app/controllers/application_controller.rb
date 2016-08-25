96f5219f1b9f6ee55d4226fdb8946b0d0337ec98
ebanh <ebanhernandez@gmail.com>
2016-08-25 13:38:57 +0900

    Added layout page, slugifiable and user login and signup actions and log in page. Also fixed typo in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
      end
 
      def current_user
-       User.find(session[:user_id])
+       @current_user ||= User.find(session[:user_id])
      end
 
      def redirect_if_not_logged_in
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,45 @@
+require 'pry'
 class UserController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb "/users/show"
+  end
+
   get '/login' do
-    if !sessions[:user_id]
+    if !logged_in?
       erb :"/users/login"
     else
-      erb :"/users/"
+      erb :"/users/#{current_user.slug}"
     end
   end
 
   post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/users/#{user.slug}"
+    else
+      redirect to "/login"
+    end
+  end
 
-    redirect to "/users/#{@user.id}"
+  get '/signup' do
+    if logged_in?
+      redirect to "/users/#{current_user.slug}"
+    else
+      erb :"/users/signup"
+    end
+  end
+
+  post '/signup' do
+    user = User.create(params)
+    session[:user_id] = user.id
+    if user.save
+      redirect to "/users/#{user.slug}"
+    else
+      redirect to "/signup"
+    end
   end
 
 end
