775d486e28d5b852d92f8a8cda93cfadf979d125
ebanh <ebanhernandez@gmail.com>
2016-08-26 23:47:17 +0900

    added the edit get route to meal controller and view. git push origin master

--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -1,10 +1,19 @@
 class MealController < ApplicationController
 
   get '/meals/new' do
+    redirect_if_not_logged_in
     erb :"/meals/new"
   end
 
+  get '/meals/:id/edit' do
+    redirect_if_not_logged_in
+    @meal = Meal.find(params[:id])
+    redirect_if_incorrect_user(@meal.user)
+    erb :"/meals/edit"
+  end
+
   get '/meals' do
+    redirect_if_not_logged_in
     @user = current_user
     erb :"/meals/index"
   end
