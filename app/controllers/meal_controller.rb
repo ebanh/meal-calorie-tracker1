02fc8e77a336972c3068524de5c8bd63ab48df41
ebanh <ebanhernandez@gmail.com>
2016-08-28 12:05:56 +0900

    added edit view and edit get and patch routes.

--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -20,8 +20,20 @@ class MealController < ApplicationController
 
   post '/meals' do
     Meal.create(params)
-
     redirect to '/meals'
+  end
+
+  patch '/meals/:id' do
+    meal = Meal.find(params[:id])
+    if params[:name].empty? && params[:calories].empty?
+      flash[:message] = "No data entered to edit"
+      redirect to "/meals/#{meal.id}/edit"
+    end
+    meal.name = params[:name] unless params[:name].empty?
+    meal.calories = params[:calories] unless params[:calories].empty?
+    meal.save
+
+    redirect to "/meals"
   end
 
 end
